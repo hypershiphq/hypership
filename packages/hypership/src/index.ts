e6ee5ec00c5a839f8192234745dde1e16ba26ecc
Jack Smith <jjsmith1116@gmail.com>
2025-01-08 22:04:20 +0000

    0.0.14 - CLI fix for NPM global installs

--- packages/hypership/src/index.ts
+++ packages/hypership/src/index.ts
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
-import('@hypership/cli').catch(err => {
-  console.error('Failed to load hypership:', err);
-  process.exit(1);
-});
+(async () => {
+  try {
+    const cli = await import('@hypership/cli');
+    if (cli.default) {
+      cli.default();
+    } else {
+      console.error('Failed to find default export in @hypership/cli.');
+      process.exit(1);
+    }
+  } catch (err) {
+    console.error('Failed to load hypership:', err);
+    process.exit(1);
+  }
+})();
