23695045933ea4ba40930f12f6e1e0dbfb734a2c
Jack Smith <167023202+hypershipdev1@users.noreply.github.com>
2025-01-09 10:05:51 +0000

    0.0.16 - hypership logout fix

--- packages/cli/src/util/logout/deleteToken.ts
+++ packages/cli/src/util/logout/deleteToken.ts
@@ -1,17 +1,16 @@
 import fs from 'fs'
-import os from 'os'
 import path from 'path'
+import XDGAppPaths from 'xdg-app-paths'
 
 export const deleteStoredToken = async () => {
-  try { 
-    const homeDir = os.homedir()
-    const configDir = path.join(homeDir, '.hypership')
-    const configFile = path.join(configDir, 'hypership.json')
+  try {
+    const directories = XDGAppPaths('com.hypership.cli').dataDirs();
+    const configFile = path.join(directories[0], 'config.json');
 
     if (fs.existsSync(configFile)) {
-      fs.unlinkSync(configFile)
+      fs.unlinkSync(configFile);
     }
   } catch (error) {
-    console.error(`Error deleting the token: ${error}`)
+    console.error('Error deleting token:', error);
   }
 }
