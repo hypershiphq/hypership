bcc402d1260b460996a4e914e5fcdb263924a606
Jay Freeman <james@hypership.dev>
2025-02-01 10:44:22 +0000

    Remove react-cookie-manager package files
    Completely remove the entire react-cookie-manager package directory, including all source files, configuration, and assets. This appears to be a cleanup operation to remove an entire package structure.

--- packages/components/react-cookie-manager/src/index.ts
+++
@@ -1,8 +0,0 @@
-import "./styles/tailwind.css";
-export { default as CookieConsenter } from "./components/CookieConsenter";
-export type { CookieConsenterProps } from "./types/types";
-export {
-  CookieManager,
-  useCookieConsent,
-} from "./context/CookieConsentContext";
-export type { CookieManagerProps } from "./context/CookieConsentContext";
